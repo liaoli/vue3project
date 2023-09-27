52403a875cc6dfb3f698ede5767c49c957bb41f6
liaoli <277218962@qq.com>
2023-09-27 11:47:06 +0800

    watch

---
+++ src/06_watch_single.vue
@@ -0,0 +1,21 @@
+
+<script setup>
+import { ref, watch } from 'vue';
+const message = ref('vue3 watch');
+const count = ref(0);
+// 1. 监视单个数据的变化
+watch(count, (newValue, oldValue) => {
+  console.log(`count 发生了变化, ${newValue}, ${oldValue}`);
+});
+
+const changCount = () => {
+  count.value++;
+};
+
+</script>
+
+<template>
+  <div>{{ message }}</div>
+  <button @click="changCount">{{ count }}</button>
+</template>
+
