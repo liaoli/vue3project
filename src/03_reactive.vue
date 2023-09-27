60a6fc4800f6085f9c1215eb821aabd869bbe01b
liaoli <277218962@qq.com>
2023-09-27 11:12:30 +0800

    init

---
+++ src/03_reactive.vue
@@ -0,0 +1,64 @@
+<!-- <script>
+export default {
+  name: "App",
+  setup () {
+    console.log('setup')
+    const message = 'Hello vue3'
+
+    const logMessage = () => {
+      console.log(message)
+    }
+
+    return {
+      message,
+      logMessage
+    };
+  },
+}
+</script> -->
+<!-- setup 语法糖 -->
+<script setup>
+import { reactive } from 'vue';
+const message = 'reactive vue3'
+
+const state = reactive({
+  count: 0
+})
+const setCount = () => {
+  state.count++
+}
+</script>
+
+<template>
+  <div>{{ message }}</div>
+  <button @click="setCount">点我{{ state.count }}</button>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+
+<script setup>
+import { ref, computed } from 'vue';
+const message = 'computed vue3'
+
+const list = ref([1,2,3,4,5,6])
+const sum = computed(() => {
+  return list.value.reduce((pre, cur) => {
+    return pre + cur
+  })
+})
+
+const largeThen4 = computed(() => {
+  return list.value.filter(item => item > 4)
+})
+</script>
+
+<template>
+  <div>{{list}}-{{ message }}</div>
+  <button >数组的和{{ sum }}</button>
+  <div>大于4的数组是{{ largeThen4 }}</div>
+</template>
+
